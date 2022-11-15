e0bea68b225dca3b4d85891018449f4badc8b108
santiagoaguero <aguerosan@gmail.com>
2022-11-15 13:25:31 -0300

    mapping notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
+
+
 
 function App() {
     return <div>
         <Header />
-        <Note />
+        {notes.map(note => (<Note key={note.key} title={note.title} content={note.content}/>))};
+        
         <Footer />
         
     </div>;
 };
 
 export default App;
+
+
+
